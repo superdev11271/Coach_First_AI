23f6cc6d1e2c4c3a6801c6fe425b8041005de77a
superdev11271 <mastersuperdev@gmail.com>
2025-09-10 13:30:40 +0200

    host 0

--- backend/wsgi.py
+++ backend/wsgi.py
@@ -1,4 +1,11 @@
-from main import flask_app  # Import your Flask app from main.py
+from ingestion import flask_app
+from telegram_bot import run_bot_in_thread
+
+def main():
+    run_bot_in_thread()
+    flask_app.run(host="0.0.0.0")
+
 
 if __name__ == "__main__":
-    flask_app.run()
+    main()
+    
