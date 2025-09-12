51df302313e3b1993cd15550011f10470bc5ae94
hercules <hercules.broken11@gmail.com>
2025-09-11 17:01:27 -0700

    update wsgi

--- backend/main.py
+++ backend/main.py
@@ -1,11 +1,15 @@
 from ingestion import flask_app
 from telegram_bot import run_bot_in_thread
 
-def main():
+def start_services():
+    # Start Telegram bot
     run_bot_in_thread()
-    flask_app.run(host="0.0.0.0", port=5000, debug=False, use_reloader=False)
+    return flask_app
 
+def main():
+    app = start_services()
+    # Start Flask app in development mode
+    app.run(host="0.0.0.0", port=5000, debug=False, use_reloader=False)
 
 if __name__ == "__main__":
     main()
-    
